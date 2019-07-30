e8011312c03dbd503eb964c01f48e310f46b61cb
Minal Kharche <minal.berkeley@gmail.com>
2019-07-29 20:24:50 -0700

    updated server.js with dependencies and routes

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+// const path = require("path");
 const PORT = process.env.PORT || 3003;
 const app = express();
-const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -13,12 +14,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
