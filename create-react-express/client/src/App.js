acabb51bace4de2bb6c7082bcc0fed59612bc200
Minal Kharche <minal.berkeley@gmail.com>
2019-07-30 18:05:23 -0700

    added layout and fixed error and added react router dom in package.json

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Jumbotron from "./components/Jumbotron";
+import NavBar from "./components/Nav";
+import Books from "./pages/Books";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <NavBar/>
+        <Jumbotron />
+        <Switch>
+          <Route exact path="/" component={Books}/>
+          <Route exact path="/saved" component={Books}/>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 // const path = require("path");
-const PORT = process.env.PORT || 3003;
 const app = express();
+const PORT = process.env.PORT || 3003;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -19,10 +20,10 @@ app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 // app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 // });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
